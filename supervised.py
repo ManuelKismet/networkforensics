07143498ab8918f9f476aa31d9c2ef0933aac2a8
Xcatter <kismetkoranteng@yahoo.com>
2024-05-14 23:40:09 +0100

    processing

--- supervised.py
+++ supervised.py
@@ -1,14 +1,14 @@
 import os
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.preprocessing import StandardScaler
 
-# DATA_DIR = os.environ['DataSetPath']  # r'C:\Users\jd Phones\Documents\Thesis\dataSet'
-index = 0
 
+# DATA_DIR = os.environ['DataSetPath']  # r'C:\Users\jd Phones\Documents\Thesis\dataSet'
 
 def myprint(*args):
     for i in range(0, len(args), 2):
@@ -29,12 +29,14 @@ def file_path():
                 pfp = os.path.join(root, file)  # pfp: parquet file path
                 path.append(pfp)
     # [print(item) for item in path]
+    print('read files')
     return path
 
 
 def read_parquet_dataset(path):
     dataset = [pd.read_parquet(item) for item in path]
     dataset = pd.concat(dataset, axis=0)
+    print('concate data')
     return dataset
 
 
@@ -48,23 +50,20 @@ def binary_labeling(data):
     label_map.update({label: 1 for label in attack_labels})
 
     data['Label'] = data['Label'].map(label_map)
-    print(data['Label'].value_counts())
+    print('labeling done')
     return data
 
 
 def data_balancing(data):
     d_count = data['Label'].value_counts()
-    print(d_count)
     min_d_label = d_count.idxmin()  # 1 as index of the min val count
-    print(min_d_label)
     min_d_count = d_count[min_d_label]  # 541
-    print(min_d_count)
     max_d_ind = data.index[data['Label'] != min_d_label]
-    print(max_d_ind)
     rand_ind = np.random.choice(max_d_ind, min_d_count, replace=False)
     max_d_samp = data.loc[rand_ind]
     u_sampled = pd.concat([data[data['Label'] == min_d_label], max_d_samp], axis=0)
     data = u_sampled
+    print('balancing done')
     return data
 
 
@@ -72,7 +71,7 @@ def train_test_data(usd):  # usd: under sampled data
     train, test = train_test_split(usd, test_size=0.3, random_state=42)
     training = train
     testing = test
-
+    print('train test split done')
     return training, testing
 
 
@@ -81,7 +80,7 @@ def xy_split(tr_data, te_data):  # training and testing data
     x_test = te_data.drop(['Label', 'Protocol'], axis=1)
     y_train = tr_data['Label']
     y_test = te_data['Label']
-
+    print('x and y assinged')
     return x_train, x_test, y_train, y_test
 
 
@@ -89,22 +88,26 @@ def scaling(xtr_data, xte_data):  # training data
     scaler = StandardScaler()
     scaled_tr_data = scaler.fit_transform(xtr_data)
     scaled_te_data = scaler.fit_transform(xte_data)
+    print('scaling done')
     return scaled_tr_data, scaled_te_data
 
 
 def model_train_test(xt_data, yt_data, xte_data, yte_data):  # training data and test data
-    params = {'max_depth': [2, 4, 7, 10], 'min_samples_split': [2, 3, 5],
-              'min_samples_leaf': [1, 2, 3, 5], 'criterion': ['gini', 'entropy']}
-    dtc = DecisionTreeClassifier(criterion='entropy', max_depth=5)  # dtc: decision tree classifier
-    # dtc.fit(xt_data[index], yt_data[index])
-    # y_pred = dtc.predict(xte_data[index])
-    # print(classification_report(yte_data[index], y_pred))
+    params = {'max_depth': [5, 10], 'min_samples_split': [2, 5],
+              'min_samples_leaf': [1, 2], 'criterion': ['gini', 'entropy'], 'random_state': [42, 0],
+              'n_estimators': [50, 100]}
+    dtc = DecisionTreeClassifier(criterion='entropy', max_depth=10, min_samples_leaf=1,
+                                 min_samples_split=5)  # dtc: decision tree classifier
+
+    dtc.fit(xt_data, yt_data)
+    y_pred = dtc.predict(xte_data)
+    print(classification_report(yte_data, y_pred))
 
     # hyper param tuning
-    rd = RandomizedSearchCV(dtc, param_distributions=params, n_iter=10, cv=3, n_jobs=-1)
-    rd.fit(xt_data, yt_data)
-    print(rd.best_params_)
-    print(rd.best_score_)
+    # rd = RandomizedSearchCV(rdf, param_distributions=params, n_iter=10, cv=3, n_jobs=-1)
+    # rd.fit(xt_data, yt_data)
+    # print(rd.best_params_)
+    # print(rd.best_score_)
 
 
 if __name__ == '__main__':
